65831e04ccb33639e29c916291a355b154b9617e
duf5 <2471510851@qq.com>
2022-12-15 11:36:45 +0000

    update

--- 1_算法示例/solution.py
+++ 1_算法示例/solution.py
@@ -2,13 +2,14 @@ from keras.layers import Input, LSTM, Dense
 from keras.models import Model, load_model
 import numpy as np
 import sys
+import tensorflow.compat.v2 as tf
 
 # 定义神经网络的参数
 batch_size = 64   # 一次训练所选取的样本数
-epochs = 5   # 训练轮数
+epochs = 500    # 训练轮数
 latent_dim = 256  # LSTM的单元个数
-num_samples = 10000   # 训练样本的大小
-data_path = '1_算法示例/datas.txt'   # 数据集路径
+num_samples = 3000   # 训练样本的大小
+data_path = '1_算法示例/new_datas.txt'   # 数据集路径
 
 # 输入（源语言）序列，即输入的中文字符串
 input_texts = []
@@ -25,10 +26,9 @@ target_characters = set()
 with open(data_path, 'r', encoding='utf-8') as f:
     lines = f.read().split('\n')
 for line in lines[: min(num_samples, len(lines) - 1)]:
-    target_text, input_text = line.split('\t')
+    input_text, target_text = line.split('\t')
     # 使用“tab”作为序列开始的特征，使用“\n”作为序列结束的特征
     target_text = '\t' + target_text + '\n'
-
     input_texts.append(input_text)
     target_texts.append(target_text)
     # 制作源语言的词典
@@ -59,11 +59,11 @@ print('Number of unique output tokens:', num_decoder_tokens)
 print('Max sequence length for inputs:', max_encoder_seq_length)
 print('Max sequence length for outputs:', max_decoder_seq_length)
 
-# 建立字符到数字的映射（字典）
+# 建立字符到数字的映射（字典），用于字符的向量化
 input_token_index = dict([(char, i) for i, char in enumerate(input_characters)])
 target_token_index = dict([(char, i) for i, char in enumerate(target_characters)])
 
-# 建立数字到字符的映射（字典）
+# 建立数字到字符的映射（字典），用于恢复
 reverse_input_char_index = dict(
     (i, char) for char, i in input_token_index.items())
 reverse_target_char_index = dict(
@@ -80,6 +80,7 @@ decoder_input_data = np.zeros(
 decoder_target_data = np.zeros(
     (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
     dtype='float32')
+
 # zip('ABC','xyz') ==> Ax By Cz
 # 将输入的中文或英文字符串进行one-hot编码
 for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
@@ -90,9 +91,7 @@ for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
         if t > 0:
             # decoder_target_data不包含开始字符tab
             decoder_target_data[i, t - 1, target_token_index[char]] = 1.
-
 # ------------------------------------------------------------------------------
-
 
 # --------------------建立模型---------------------------------------------------
 def create_model():
@@ -112,7 +111,6 @@ def create_model():
     decoder_lstm = LSTM(latent_dim, return_state=True, return_sequences=True)
     # 将编码器输出的状态作为初始解码器的初始状态
     decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_state)
-
     # 添加全连接层，将输出全连接到num_decoder_tokens（所有的不重复的英文字符）维度上，
     # 激活函数选用'softmax',根据得到的每个字符的概率，判断decoder每一步的输出是什么
     decoder_dense = Dense(num_decoder_tokens, activation='softmax')
@@ -149,16 +147,13 @@ def decode_sequence(input_seq, encoder_model, decoder_model):
     target_seq = np.zeros((1, 1, num_decoder_tokens))
     # 将这个空序列的内容设置为开始字符'\t'
     target_seq[0, 0, target_token_index['\t']] = 1.
-    
+
     stop_condition = False
     decoded_sentence = ''
     while not stop_condition:
         # 以'\t'为开头，结合状态向量states_value，开始一个一个向后预测
         output_tokens, h, c = decoder_model.predict([target_seq] + states_value)
         # output_tokens是下一个字符出现的位置概率
-        # print("预测下一个字符出现的概率：", output_tokens)
-        # print("解码器隐状态：", h)
-        # print("上下文向量：", c)
 
         # 对下个字符采样  sampled_token_index是要预测下个字符最大概率出现在字典中的位置
         sampled_token_index = np.argmax(output_tokens[0, -1, :])
@@ -169,7 +164,7 @@ def decode_sequence(input_seq, encoder_model, decoder_model):
         decoded_sentence += sampled_char
 
         # 退出条件：生成 \n 或者 超过最大序列长度
-        if (sampled_char == '\n' or len(decoded_sentence) > max_decoder_seq_length):
+        if sampled_char == '\n' or len(decoded_sentence) > max_decoder_seq_length:
             stop_condition = True
 
         # 更新target_seq，将新预测得到的字符添加到target_seq中，作为下一轮预测的输入
@@ -186,12 +181,12 @@ def decode_sequence(input_seq, encoder_model, decoder_model):
 def train():
     model, encoder_model, decoder_model = create_model()
     # 编译模型
-    model.compile(optimizer='rmsprop',loss='categorical_crossentropy')
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-2), loss='categorical_crossentropy')
     # 训练模型
     model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
               batch_size=batch_size,
               epochs=epochs,
-              validation_split=0.2)
+              validation_split=0.1)
 
     # 保存模型，方便测试使用
     model.save('model/s2s.h5')
@@ -206,10 +201,8 @@ def test():
     if ss == '-1':
         sys.exit()
     input_seq = np.zeros((1, max_encoder_seq_length, num_encoder_tokens))
-    print("input_seq:", input_seq)
     for t, char in enumerate(ss):
         input_seq[0, t, input_token_index[char]]=1.0
-    print("input_seq:", input_seq)
     decoded_sentence = decode_sequence(input_seq, encoder_model, decoder_model)
     print('-')
     print('Decoded sentence:', decoded_sentence)
