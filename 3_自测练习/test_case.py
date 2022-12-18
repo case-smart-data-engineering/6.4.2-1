b7f0675fc6680d9bd3ce44a3fa90cbb40f62f597
duf5 <2471510851@qq.com>
2022-12-18 16:24:57 +0000

    update

---
+++ 3_自测练习/test_case.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from my_solution import *
+
+# 测试用例
+def test_solution():
+    train()
+    result = test()
+    
+    # 正确答案
+    correct_solution = 'unbelievable'
+    
+    # 程序求解结果
+    assert correct_solution == result
